9940a817c4ad63e1e34d13242030929a8301027a
jongho <grit1life@gmail.com>
2021-07-04 21:41:21 +0900

    알고리즘

---
+++ JavaPractice/src/algo/Max.java
@@ -0,0 +1,21 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Max {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int val, max=0, order=0;
+		for(int i=0; i<9; i++) {
+			val = sc.nextInt();
+			if(val > max) {
+				max = val;
+				order = i+1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(order);
+	}
+
+}
