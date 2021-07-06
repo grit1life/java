b8130dd00e64d29a9d11f1fef0fc3c85f1b3f11d
jongho <grit1life@gmail.com>
2021-07-06 21:15:48 +0900

    알고리즘 연습

---
+++ JavaPractice/src/algo/Ascii.java
@@ -0,0 +1,14 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char val = sc.nextLine().charAt(0);
+		int num = (int)val;
+		System.out.println(num);
+	}
+
+}
